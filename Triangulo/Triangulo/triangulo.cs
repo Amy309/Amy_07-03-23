1dcfe62580356189e0f4c931f0662655b32b3af5
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-07 21:20:32 -0600

    Codigo

---
+++ Triangulo/Triangulo/triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    public class triangulo
+    {
+        public void Triangulo(string[] args)
+        {
+            Console.Write("Introduce un número entero para la altura");
+            int altura = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= altura; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
